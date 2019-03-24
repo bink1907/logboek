879bc08d0595d80fe883cac05235cc8d0a76c95d
Bink de Kievit <bink@thisisdevelopment.nl>
2019-03-25 00:08:05 +0100

    Add all

---
+++ src/Entity/Logboek.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LogboekRepository")
+ */
+class Logboek
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $userId;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $brief_nr;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $datum;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $chauffeurId;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Truck")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $truckId;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $kubs;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $laadplaats;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $vertrektijd;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $bestemming;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $evenement;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): self
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    public function getBriefNr(): ?int
+    {
+        return $this->brief_nr;
+    }
+
+    public function setBriefNr(int $brief_nr): self
+    {
+        $this->brief_nr = $brief_nr;
+
+        return $this;
+    }
+
+    public function getDatum(): ?\DateTimeInterface
+    {
+        return $this->datum;
+    }
+
+    public function setDatum(\DateTimeInterface $datum): self
+    {
+        $this->datum = $datum;
+
+        return $this;
+    }
+
+    public function getChauffeurId(): ?int
+    {
+        return $this->chauffeurId;
+    }
+
+    public function setChauffeurId(?int $chauffeurId): self
+    {
+        $this->chauffeurId = $chauffeurId;
+
+        return $this;
+    }
+
+    public function getTruckId(): ?Truck
+    {
+        return $this->truckId;
+    }
+
+    public function setTruckId(?Truck $truckId): self
+    {
+        $this->truckId = $truckId;
+
+        return $this;
+    }
+
+    public function getKubs(): ?int
+    {
+        return $this->kubs;
+    }
+
+    public function setKubs(int $kubs): self
+    {
+        $this->kubs = $kubs;
+
+        return $this;
+    }
+
+    public function getLaadplaats(): ?string
+    {
+        return $this->laadplaats;
+    }
+
+    public function setLaadplaats(string $laadplaats): self
+    {
+        $this->laadplaats = $laadplaats;
+
+        return $this;
+    }
+
+    public function getVertrektijd(): ?\DateTimeInterface
+    {
+        return $this->vertrektijd;
+    }
+
+    public function setVertrektijd(\DateTimeInterface $vertrektijd): self
+    {
+        $this->vertrektijd = $vertrektijd;
+
+        return $this;
+    }
+
+    public function getBestemming(): ?string
+    {
+        return $this->bestemming;
+    }
+
+    public function setBestemming(string $bestemming): self
+    {
+        $this->bestemming = $bestemming;
+
+        return $this;
+    }
+
+    public function getEvenement(): ?string
+    {
+        return $this->evenement;
+    }
+
+    public function setEvenement(string $evenement): self
+    {
+        $this->evenement = $evenement;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Truck.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TruckRepository")
+ */
+class Truck
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $kenteken;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $merk;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $bouwjaar;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getKenteken(): ?string
+    {
+        return $this->kenteken;
+    }
+
+    public function setKenteken(string $kenteken): self
+    {
+        $this->kenteken = $kenteken;
+
+        return $this;
+    }
+
+    public function getMerk(): ?string
+    {
+        return $this->merk;
+    }
+
+    public function setMerk(string $merk): self
+    {
+        $this->merk = $merk;
+
+        return $this;
+    }
+
+    public function getBouwjaar(): ?int
+    {
+        return $this->bouwjaar;
+    }
+
+    public function setBouwjaar(int $bouwjaar): self
+    {
+        $this->bouwjaar = $bouwjaar;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        // TODO: Implement __toString() method.
+        return $this->getKenteken();
+    }
+}
